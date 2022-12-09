2a72c607b0553b192d09a6722f4a1db75b14b088
Your Name <you@example.com>
2022-12-09 11:10:05 +0700

    init and use variables

---
+++ initialize-using-variables.js
@@ -0,0 +1,27 @@
+let i = 10
+let f = 20.5
+let b = true
+let s = 'Hà Nội'
+
+document.write('i = ' + i)
+document.write('<br/>')
+document.write('f = ' + f)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br/>')
+document.write('s = ' + s)
+
+let width = 20
+let height = 10
+let area = width * height
+document.write('<br>')
+document.write('Area = ' + area)
+a=prompt("Nhập Biến a");
+b=prompt("Nhập Biến b");
+if (a%b ==0) {
+    alert("a chia hết cho b");
+}
+    else {
+        alert("a không chia hết cho b");
+    }
+
